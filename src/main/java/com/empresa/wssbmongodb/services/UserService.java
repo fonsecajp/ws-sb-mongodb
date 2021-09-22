099676977e0d8d748e66a7caaf65eaf2b30bdd4e
Joao Fonseca <jopalves39@gmail.com>
2021-09-22 16:36:03 -0300

    Obter usuario por id e exceptions

--- src/main/java/com/empresa/wssbmongodb/services/UserService.java
+++ src/main/java/com/empresa/wssbmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.empresa.wssbmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.empresa.wssbmongodb.domain.User;
 import com.empresa.wssbmongodb.repository.UserRepository;
+import com.empresa.wssbmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
---
+++ src/main/java/com/empresa/wssbmongodb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.empresa.wssbmongodb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException (String msg) {
+		super(msg);
+	}
+
+}
