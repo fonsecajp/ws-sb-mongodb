55ac209a38cfe65719831701abe2e6f2d6343621
Joao Fonseca <jopalves39@gmail.com>
2021-09-22 10:41:22 -0300

    Primeiro comit - projeto criado

---
+++ src/main/java/com/empresa/wssbmongodb/WssbmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.wssbmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WssbmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WssbmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/wssbmongodb/WssbmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.wssbmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WssbmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
