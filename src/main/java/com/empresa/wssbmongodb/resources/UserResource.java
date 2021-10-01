db2039e618d570b1e25f358fbb22a5b3b14084ed
Joao Fonseca <jopalves39@gmail.com>
2021-10-01 10:43:23 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/empresa/wssbmongodb/resources/UserResource.java
+++ src/main/java/com/empresa/wssbmongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.empresa.wssbmongodb.domain.Post;
 import com.empresa.wssbmongodb.domain.User;
 import com.empresa.wssbmongodb.dto.UserDTO;
 import com.empresa.wssbmongodb.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = userService.update(obj);
 		return ResponseEntity.noContent().build(); 
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
