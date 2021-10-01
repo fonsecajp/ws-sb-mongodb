ed9322ce5e92906b81db3ff6db153c2277613dec
fonsecajp <jopalves39@gmail.com>
2021-10-01 12:50:16 -0300

    Consulta com vários critérios

--- src/main/java/com/empresa/wssbmongodb/repository/PostRepository.java
+++ src/main/java/com/empresa/wssbmongodb/repository/PostRepository.java
@@ -1,5 +1,8 @@
 package com.empresa.wssbmongodb.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,11 +12,13 @@ import com.empresa.wssbmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
-<<<<<<< HEAD
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
-=======
->>>>>>> bda7931ed228055377fc377764685544c83cd874
+	
+	
+	@Query("{ $and: [ {date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
+	
 }
